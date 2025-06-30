322f9649f4bf3ef71f521cb2d7324cccd9b6e0dc
Kim-Dongdong <kdssos11@naver.com>
2025-06-30 14:47:37 +0900

    공통 레이아웃 추가

---
+++ src/main/java/com/grabpt/GrabptApplication.java
@@ -0,0 +1,13 @@
+package com.grabpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabptApplication.class, args);
+	}
+
+}

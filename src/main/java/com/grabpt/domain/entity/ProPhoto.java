e9018040466ca099c405c3d6bb8c53ef5751ffd3
Park Young Jin <dydwls2227@naver.com>
2025-07-07 01:53:48 +0900

    전문가 사용자 관련 엔터티 구현

---
+++ src/main/java/com/grabpt/domain/entity/ProPhoto.java
@@ -0,0 +1,36 @@
+package com.grabpt.domain.entity;
+
+import com.grabpt.domain.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ProPhoto extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "pro_profile_id")
+	private ProProfile proProfile;
+
+	@Column(nullable = false)
+	private String imageUrl; // 업로드된 이미지 URL
+
+	private String description; // 사진 설명
+}

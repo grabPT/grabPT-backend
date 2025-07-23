de816b5d1e70b363b4c65f7d41b1c4cad2a8a32d
Kim-Dongdong <kdssos11@naver.com>
2025-07-23 16:26:29 +0900

    배포 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,19 @@
+#!/usr/bin/env bash
+
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/app.jar"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date "+%Y-%m-%d %H:%M:%S")
+TIME_NOW=$(date +%c)
 
-CURRENT_PID=$(pgrep -f "$JAR_FILE")
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-if [ -z "$CURRENT_PID" ]; then
-  echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
-  echo "$TIME_NOW > 실행 중인 애플리케이션(PID: $CURRENT_PID) 종료" >> $DEPLOY_LOG
-  kill -15 "$CURRENT_PID"
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
 fi

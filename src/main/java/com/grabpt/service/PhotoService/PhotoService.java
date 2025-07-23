1b3565d450d6490157efd8da93b53aedc803f07d
sammool <sammool2003@gmail.com>
2025-07-23 11:45:50 +0900

    임시 저장

--- src/main/java/com/grabpt/service/PhotoService/PhotoService.java
+++ src/main/java/com/grabpt/service/PhotoService/PhotoService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.grabpt.domain.entity.ProProfile;
 import com.grabpt.dto.request.PhotoRequestDTO;
+import com.grabpt.dto.response.ChatResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface PhotoService {
 	void updatePhotos(ProProfile proProfile, List<PhotoRequestDTO> photoDTOs);

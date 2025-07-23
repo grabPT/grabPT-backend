3dc3664c496af1630f5d8b6223d6a986c854fe6d
Kim-Dongdong <kdssos11@naver.com>
2025-07-23 15:35:55 +0900

    배포 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-
-PROJECT_ROOT="/home/ubuntu"
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_NAME=$(ls $PROJECT_ROOT/*.jar | grep -v 'app.jar' | head -n 1)
 JAR_FILE="$PROJECT_ROOT/app.jar"
-ENV_FILE="$PROJECT_ROOT/.env.properties"  # 추가
+ENV_FILE="$PROJECT_ROOT/.env.properties"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -10,12 +9,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date "+%Y-%m-%d %H:%M:%S")
 
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+# 기존 jar를 새 파일로 덮어쓰기
+echo "$TIME_NOW > JAR 파일 복사: $JAR_NAME → $JAR_FILE" >> $DEPLOY_LOG
+cp "$JAR_NAME" "$JAR_FILE"
 
-# jar 파일 실행 (환경설정 포함)
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE --spring.config.import=optional:file:$ENV_FILE > $APP_LOG 2> $ERROR_LOG &
+# jar 실행
+echo "$TIME_NOW > JAR 파일 실행: $JAR_FILE" >> $DEPLOY_LOG
+nohup java -jar "$JAR_FILE" --spring.config.import=optional:file:"$ENV_FILE" > "$APP_LOG" 2> "$ERROR_LOG" &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f "$JAR_FILE")
+echo "$TIME_NOW > 실행된 프로세스 ID: $CURRENT_PID" >> $DEPLOY_LOG

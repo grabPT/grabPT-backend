1541224b3be1bc3a610bec104b60cf270bfc9af8
Kim-Dongdong <kdssos11@naver.com>
2025-08-07 12:18:56 +0900

    requestionId 반환

--- src/main/java/com/grabpt/domain/enums/Gender.java
+++ src/main/java/com/grabpt/domain/enums/Gender.java
@@ -1,5 +1,24 @@
 package com.grabpt.domain.enums;
 
 public enum Gender {
-	MALE, FEMALE
+	MALE("남자"),
+	FEMALE("여자");
+
+	private final String korean;
+
+	Gender(String korean) {
+		this.korean = korean;
+	}
+
+	public String getKorean() {
+		return korean;
+	}
+
+	public static Gender fromKorean(String korean) {
+		if ("남자".equals(korean))
+			return MALE;
+		if ("여자".equals(korean))
+			return FEMALE;
+		throw new IllegalArgumentException("성별은 '남자' 또는 '여자'만 입력해주세요.");
+	}
 }

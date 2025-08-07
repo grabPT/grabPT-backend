9e733b25f0393c191e4250b26acc0f5b2050d973
Kim-Dongdong <kdssos11@naver.com>
2025-08-07 13:08:59 +0900

    mySuggestion에 profileImageUrl 추가

--- src/main/java/com/grabpt/dto/response/SuggestionResponseDto.java
+++ src/main/java/com/grabpt/dto/response/SuggestionResponseDto.java
@@ -57,6 +57,7 @@ public class SuggestionResponseDto {
 		private Integer price;
 		private Integer sessionCount;
 		private RequestStatus status;
+		private String profileImageUrl;
 		private Long requestionId;
 	}
 

c9011be03f8a0807c74d01592464f5565a14a941
Kim-Dongdong <kdssos11@naver.com>
2025-08-08 22:08:17 +0900

    웹소켓 설정

--- src/main/java/com/grabpt/service/SuggestionService/SuggestionServiceImpl.java
+++ src/main/java/com/grabpt/service/SuggestionService/SuggestionServiceImpl.java
@@ -49,32 +49,6 @@ public class SuggestionServiceImpl implements SuggestionService {
 	private final UserQueryService userQueryService;
 	private final AmazonS3Manager amazonS3Manager;
 
-	// @Override
-	// public Suggestions save(SuggestionRequestDto dto, String email) {
-	// 	Users user = userRepository.findByEmail(email)
-	// 		.orElseThrow(() -> new UserHandler(ErrorStatus.MEMBER_NOT_FOUND));
-	//
-	// 	ProProfile proProfile = proProfileRepository.findByUser(user)
-	// 		.orElseThrow(() -> new ProHandler(ErrorStatus.PRO_NOT_FOUND));
-	//
-	// 	Requestions requestion = requestionRepository.findById(dto.getRequestionId())
-	// 		.orElseThrow(() -> new RequestionHandler(ErrorStatus.REQUESTION_NOT_FOUND));
-	//
-	// 	Suggestions suggestion = Suggestions.builder()
-	// 		.price(dto.getPrice())
-	// 		.sessionCount(dto.getSessionCount())
-	// 		.message(dto.getMessage())
-	// 		.location(dto.getLocation())
-	// 		.sentAt(dto.getSentAt() != null ? dto.getSentAt() : LocalDate.now())
-	// 		.isAgreed(dto.getIsAgreed() != null ? dto.getIsAgreed() : false)
-	// 		.build();
-	//
-	// 	suggestion.setProProfile(proProfile);   // 연관관계 설정
-	// 	suggestion.setRequestion(requestion);   // 연관관계 설정
-	//
-	// 	return suggestionRepository.save(suggestion);
-	// }
-
 	@Override
 	public Suggestions save(SuggestionRequestDto dto, String email, List<MultipartFile> photos) {
 		Users user = userRepository.findByEmail(email)

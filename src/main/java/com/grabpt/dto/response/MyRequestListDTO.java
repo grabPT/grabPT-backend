d48e04ab5d1c961de1afd02b21e6e325f94391c3
Park Young Jin <dydwls2227@naver.com>
2025-08-12 19:12:03 +0900

    주소 DTO 포함

--- src/main/java/com/grabpt/dto/response/MyRequestListDTO.java
+++ src/main/java/com/grabpt/dto/response/MyRequestListDTO.java
@@ -1,33 +1,59 @@
 package com.grabpt.dto.response;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.grabpt.domain.entity.Address;
 import com.grabpt.domain.entity.Requestions;
 
+import lombok.Builder;
 import lombok.Getter;
 @Getter
 public class MyRequestListDTO {
 	private Long requestId;
 	private String imageURL;
 	private Long userId;
-	private String location;
 	private List<String> availableDays;
 	private List<String> availableTimes;
 	private String categoryName;
 	private Integer sessionCount;
 	private String content;
+	private List<AddressDTO> address;
+
+	@Getter
+	@Builder
+	public static class AddressDTO {
+		private String city;
+		private String district;
+		private String street;
+		private String zipcode;
+		private String specAddress;
+
+		public static AddressDTO from(Address address) {
+			return AddressDTO.builder()
+				.city(address.getCity())
+				.district(address.getDistrict())
+				.street(address.getStreet())
+				.zipcode(address.getZipcode())
+				.specAddress(address.getSpecAddress())
+				.build();
+		}
+	}
 
 	public MyRequestListDTO(Requestions requestion) {
 		this.requestId = requestion.getId();
 		this.imageURL = requestion.getUser().getProfileImageUrl();
 		this.userId = requestion.getUser().getId();
-		this.location = requestion.getLocation();
+
+		// 이 부분을 수정했습니다.
+		Address addresses = requestion.getUser().getAddress();
+		this.address = Collections.singletonList(AddressDTO.from(addresses));
 		this.availableDays = requestion.getAvailableDays();
 		this.availableTimes = requestion.getAvailableTimes();
 		this.categoryName = requestion.getCategory().getName();
 		this.sessionCount = requestion.getSessionCount();
 		this.content = requestion.getContent();
-
 	}
 
 }

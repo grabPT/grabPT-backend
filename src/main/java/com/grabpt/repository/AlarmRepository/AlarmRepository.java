53731c0505769ab659bbb4ecd4ecbd8bef9fc6af
sammool <sammool2003@gmail.com>
2025-08-14 01:41:14 +0900

    [Feat] 알림 읽을 시에도 안읽은 알림 개수 웹소켓으로 전달

--- src/main/java/com/grabpt/repository/AlarmRepository/AlarmRepository.java
+++ src/main/java/com/grabpt/repository/AlarmRepository/AlarmRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 	@Query("SELECT a FROM Alarm a WHERE a.user.id = :userId AND a.isRead = false")
 	List<Alarm> findAllUnReadAlarmByUserId(Long userId); //읽지 않은 것들만 조회
+
+	@Query("SELECT COUNT(a) FROM Alarm a WHERE a.user.id = :userId AND a.isRead = false")
+	Long countUnReadAlarmByUserId(Long userId);
 }

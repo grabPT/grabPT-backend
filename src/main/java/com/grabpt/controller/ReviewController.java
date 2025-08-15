0070f16391b15c2b9facdb88a7ab27f661dafdaf
Kim-Dongdong <kdssos11@naver.com>
2025-08-15 11:09:06 +0900

    리뷰 작성 userid 수정

--- src/main/java/com/grabpt/controller/ReviewController.java
+++ src/main/java/com/grabpt/controller/ReviewController.java
@@ -1,6 +1,5 @@
 package com.grabpt.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.grabpt.apiPayload.ApiResponse;
 import com.grabpt.dto.request.ReviewRequestDTO;
+import com.grabpt.dto.response.UserResponseDto;
 import com.grabpt.service.ReviewService.ReviewService;
+import com.grabpt.service.UserService.UserQueryService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,11 +20,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewController {
 	private final ReviewService reviewService;
+	private final UserQueryService userQueryService;
 
 	@Operation(summary = "리뷰 작성 API", description = "사용자가 전문가에 대한 리뷰를 작성합니다.")
 	@PostMapping
-	public ApiResponse<String> addReview(@AuthenticationPrincipal(expression = "user.id") Long userId
-	,@RequestBody ReviewRequestDTO reviewRequestDTO) {
+	public ApiResponse<String> addReview(HttpServletRequest request
+		, @RequestBody ReviewRequestDTO reviewRequestDTO) throws IllegalAccessException {
+		UserResponseDto.UserInfoDTO userInfo = userQueryService.getUserInfo(request);
+		Long userId = userInfo.getUserId();
 		reviewService.createReview(userId, reviewRequestDTO);
 		return ApiResponse.onSuccess("리뷰가 성공적으로 등록되었습니다.");
 	}

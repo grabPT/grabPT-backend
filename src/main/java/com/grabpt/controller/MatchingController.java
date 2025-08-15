f6b801bc042b4325fbad1d839e27ba70f72ea3e7
Kim-Dongdong <kdssos11@naver.com>
2025-08-15 12:17:58 +0900

    매칭 상태 변경 이슈

--- src/main/java/com/grabpt/controller/MatchingController.java
+++ src/main/java/com/grabpt/controller/MatchingController.java
@@ -1,6 +1,6 @@
 package com.grabpt.controller;
 
-import com.grabpt.dto.response.ContractResponse;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.grabpt.apiPayload.ApiResponse;
 import com.grabpt.domain.entity.Matching;
 import com.grabpt.dto.request.MatchingStatusUpdateRequest;
+import com.grabpt.dto.response.ContractResponse;
 import com.grabpt.service.MatchingService.MatchingService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,13 +24,16 @@ public class MatchingController {
 
 	private final MatchingService matchingService;
 
+	@Transactional
 	@PostMapping
 	@Operation(summary = "요청-제안 매칭 API", description = "요청서와 제안서 기반으로 매칭 생성(matchingId, contractId 반환)")
-	public ApiResponse<ContractResponse.CreateMatchingAndContractResponseDto> createMatching(@RequestParam Long requestionId, @RequestParam Long suggestionId) {
+	public ApiResponse<ContractResponse.CreateMatchingAndContractResponseDto> createMatching(
+		@RequestParam Long requestionId, @RequestParam Long suggestionId) {
 		matchingService.createMatching(requestionId, suggestionId);
 		return ApiResponse.onSuccess(matchingService.createMatching(requestionId, suggestionId));
 	}
 
+	@Transactional
 	@PostMapping("/{matchingId}/status")
 	@Operation(summary = "매칭 상태 변경 API", description = "매칭 상태를 CANCELLED 또는 COMPLETED로 변경")
 	public ApiResponse<String> updateMatchingStatus(

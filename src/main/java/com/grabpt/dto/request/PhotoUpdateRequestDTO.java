129ffe895eb42fcb329f607517074d6c42eed458
Park Young Jin <dydwls2227@naver.com>
2025-08-17 01:34:18 +0900

    certproPhoto iPart 재구조화

--- src/main/java/com/grabpt/dto/request/PhotoUpdateRequestDTO.java
+++ src/main/java/com/grabpt/dto/request/PhotoUpdateRequestDTO.java
@@ -7,6 +7,5 @@ import lombok.Getter;
 @Getter
 public class PhotoUpdateRequestDTO {
 
-	@Valid
-	private List<PhotoRequestDTO> photos;
+	private List<String> existingPhotoUrls;
 }
--- src/main/java/com/grabpt/service/PhotoService/PhotoService.java
+++ src/main/java/com/grabpt/service/PhotoService/PhotoService.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import com.grabpt.domain.entity.ProProfile;
 import com.grabpt.dto.request.PhotoRequestDTO;
+import com.grabpt.dto.request.PhotoUpdateRequestDTO;
 import com.grabpt.dto.response.ChatResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface PhotoService {
 	String uploadProfileImage(MultipartFile profileImage);
-	void updateProPhotos(ProProfile proProfile, List<MultipartFile> photoFiles);
+	void updateProPhotos(ProProfile proProfile, PhotoUpdateRequestDTO request, List<MultipartFile> newPhotoFiles);
 }

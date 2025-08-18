a0a3e08ffd75509f92d6c04d1d80329a56a3f290
Kim-Dongdong <kdssos11@naver.com>
2025-08-18 14:30:44 +0900

    description 수정

--- src/main/java/com/grabpt/domain/entity/ProProfile.java
+++ src/main/java/com/grabpt/domain/entity/ProProfile.java
@@ -1,16 +1,26 @@
 package com.grabpt.domain.entity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import jakarta.persistence.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import com.grabpt.domain.common.BaseEntity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,6 +49,7 @@ public class ProProfile extends BaseEntity {
 
 	private Integer age;
 
+	@Column(columnDefinition = "TEXT")
 	private String description; // 소개
 
 	private String programDescription; // 프로그램 상세 설명

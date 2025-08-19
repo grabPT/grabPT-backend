fcf20982eb936afff95c171e639e0a72d780ce8b
Kim-Dongdong <kdssos11@naver.com>
2025-08-19 15:38:00 +0900

    requestionService 알람 수정

--- src/main/java/com/grabpt/service/RequestionService/RequestionServiceImpl.java
+++ src/main/java/com/grabpt/service/RequestionService/RequestionServiceImpl.java
@@ -3,10 +3,6 @@ package com.grabpt.service.RequestionService;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.grabpt.domain.entity.ProProfile;
-import com.grabpt.dto.response.CategoryResponse;
-import com.grabpt.service.AlarmService.AlarmService;
-import com.grabpt.service.ProfileService.ProfileService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -28,6 +24,8 @@ import com.grabpt.dto.response.UserResponseDto;
 import com.grabpt.repository.CategoryRepository.CategoryRepository;
 import com.grabpt.repository.RequestionRepository.RequestionRepository;
 import com.grabpt.repository.UserRepository.UserRepository;
+import com.grabpt.service.AlarmService.AlarmService;
+import com.grabpt.service.ProfileService.ProfileService;
 import com.grabpt.service.UserService.UserQueryService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -79,10 +77,11 @@ public class RequestionServiceImpl implements RequestionService {
 		requestion.setUser(user); // 연관관계 설정
 		Requestions save = requestionRepository.save(requestion);
 
-		List<ProProfile> proProfiles = profileService.findAllProByCategoryCodeAndRegion(category.getCode(), requestion.getLocation());
+		List<ProProfile> proProfiles = profileService.findAllProByCategoryCodeAndRegion(category.getCode(),
+			requestion.getLocation());
 		for (ProProfile proProfile : proProfiles) {
-			alarmService.sendAlarm(proProfile.getUser().getId(),"REQUESTION", "요청서 도착",
-				requestion.getUser().getNickname()+"님의 요청서가 도착했습니다.", "/api/requestion/"+requestion.getId());
+			alarmService.sendAlarm(proProfile.getUser().getId(), "REQUESTION", "요청서 도착",
+				requestion.getUser().getNickname() + "님의 요청서가 도착했습니다.", "/matching/requests/" + requestion.getId());
 
 		}
 		return save;

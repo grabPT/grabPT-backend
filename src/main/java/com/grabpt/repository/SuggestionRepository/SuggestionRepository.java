e104c27052b3abb54d67de742f123aa53378b661
Kim-Dongdong <kdssos11@naver.com>
2025-08-21 17:02:04 +0900

    [Fix] 요청서 제안서 삭제 예외 추가

--- src/main/java/com/grabpt/repository/SuggestionRepository/SuggestionRepository.java
+++ src/main/java/com/grabpt/repository/SuggestionRepository/SuggestionRepository.java
@@ -1,8 +1,15 @@
 package com.grabpt.repository.SuggestionRepository;
 
+import static jakarta.persistence.LockModeType.*;
+
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.grabpt.domain.entity.Suggestions;
 
@@ -10,4 +17,8 @@ public interface SuggestionRepository extends JpaRepository<Suggestions, Long> {
 	Page<Suggestions> findByRequestionId(Long requestionId, Pageable pageable);
 
 	Page<Suggestions> findByProProfile_User_Email(String email, Pageable pageable);
+	
+	@Lock(PESSIMISTIC_WRITE)
+	@Query("select s from Suggestions s where s.id = :id")
+	Optional<Suggestions> findByIdForUpdate(@Param("id") Long id);
 }

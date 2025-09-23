f1bc5ebadbf21e6db1b1157fcf185c45fa131d44
sammool <sammool2003@gmail.com>
2025-09-23 17:40:42 +0900

    [Feat] cors주소 추가

--- src/main/java/com/grabpt/config/SecurityConfig.java
+++ src/main/java/com/grabpt/config/SecurityConfig.java
@@ -170,7 +170,8 @@ public class SecurityConfig {
 			"http://127.0.0.1:5173",
 			"http://localhost:3000",
 			"http://127.0.0.1:3000",
-			"https://grabpt-dev.vercel.app"
+			"https://grabpt-dev.vercel.app",
+			"https://dev-grabpt.vercel.app"
 		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		configuration.setAllowedHeaders(List.of("*"));

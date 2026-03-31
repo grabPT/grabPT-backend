9dfc2cae80ee5103ff52952291702b317c126c24
Kim-Dongdong <kdssos11@naver.com>
2026-03-31 14:36:25 +0900

    feat: 트레이너 검색 Custom Repository 구현 (JPA Criteria API)

--- src/main/java/com/grabpt/repository/ProProfileRepository/ProProfileRepository.java
+++ src/main/java/com/grabpt/repository/ProProfileRepository/ProProfileRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProProfileRepository extends JpaRepository<ProProfile, Long> {
+public interface ProProfileRepository extends JpaRepository<ProProfile, Long>, ProProfileRepositoryCustom {
 	Optional<ProProfile> findByUser(Users user);
 	@Query("""
     SELECT p
